badc1032caefb98a25b14f03d0ef00917ba36774
Antonin SIMON <antonin.simon@epsi.fr>
2023-01-11 00:34:03 +0100

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import './Home.css'
+export default function Home() {
+    return(
+        <div className="image-container">
+            <img className="image" src={require("./assets/img/Don't walk away.jpg")} alt="Don't walk away"/>
+        </div>
+    )
+}
